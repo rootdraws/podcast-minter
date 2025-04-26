15ccf29bd7b6fc62c2656f7d269f1c739448ac43
rootdraws <rootdraws@gmail.com>
2025-04-26 15:54:20 -0500

    Functional?

--- config/contract.ts
+++ config/contract.ts
@@ -1,3 +1,3 @@
 // Contract configuration
 export const CONTRACT_ADDRESS = '0x0D70EDB990D452787c77a7068ee4B41014331c2F';
-export const TOKEN_URI = 'ipfs://placeholder-token-uri'; 
+export const TOKEN_URI = 'https://i77e5vczhdiydsntjo2wdaljpr6hwcajxl3lm3rdu36wd7e7ksza.arweave.net/R_5O1Fk40YHJs0u1YYFpfHx7CAm69rZuI6b9YfyfVLI/ArbitrumMek.json'; 
